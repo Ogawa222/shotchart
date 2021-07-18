7d3a1eb798b73a7dba08f656a000258c27c788a2
Ogawa222 <72852366+Ogawa222@users.noreply.github.com>
2021-07-19 01:14:46 +0900

    jsonデータをcsvで出力する機能を追加

--- Court.js
+++ Court.js
@@ -7,6 +7,8 @@ let change = 1;
 
 //履歴を保管する配列
 const history = [];
+//座標データのjson
+const record = [];
 //初期状態の保管
 history.push(context.getImageData(0,0,canvas.width,canvas.height));
 //cancelボタン押下で履歴取得
@@ -16,14 +18,20 @@ $('#cancel').click(function(e){
         history.pop();
         id = history.pop();
         context.putImageData(id,0,0);
+        record.pop();
+        console.log(record);
         change = -1;
     }else if(history.length > 1){
         id = history.pop();
         context.putImageData(id,0,0);
+        record.pop();
+        console.log(record);
         change = -1
     }else if(history.length === 1){
         id = history[0];
         context.putImageData(id,0,0);
+        record.pop();
+        console.log(record);
         change = -1;
     }
 })
@@ -65,7 +73,9 @@ function drawShot(x,y){
     //履歴取得
     history.push(context.getImageData(0,0,canvas.width,canvas.height));
     change = 1;
-    console.log(history);
+    //座標登録
+    record.push({player:playerNumber,x_axis:x,y_axis:y,shot_mode:mode});
+    console.log(record);
 }
 
 // イベントリスナーを補足すると。その要素内で発生したイベントを補足できる
