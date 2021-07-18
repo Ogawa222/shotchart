768fb951afc154f88fcf12b0b82096cd8b975c45
Ogawa222 <72852366+Ogawa222@users.noreply.github.com>
2021-07-18 16:15:09 +0900

    クッキー機能更新

--- Court.js
+++ Court.js
@@ -77,7 +77,9 @@ const panel = document.getElementById('privacy-panel');
 document.getElementById('agreebtn').onclick = function(){
     if(agree === 'yes'){
         document.body.removeChild(panel);
+        console.log('クッキーを確認しました。');
     }else{
+        console,log('区クッキーを確認できません。')
         Cookies.set('cookie-agree', 'yes', {expires:1});
         document.body.removeChild(panel);
     }
